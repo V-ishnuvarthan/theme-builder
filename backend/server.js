88c1e17310a62ee2947b82107b6c81469f7c4189
Vishnu Varthan <vishnuvarthan88707@gmail.com>
2024-03-14 11:56:36 +0530

    updating index.js

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const cors = require('cors');
+import express, { json } from 'express';
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(cors());
-app.use(express.json());
+app.use(json());
 
 app.post('/save-theme-settings', (req, res) => {
   const { themeSettings } = req.body;
